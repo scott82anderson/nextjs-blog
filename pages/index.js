0c9028eef5d2b4d98d9bf3b491964990756ecea2
Scott Anderson <mail@scottanderson.com.au>
2022-08-02 19:21:25 +1000

    add cache-control headers for testing

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+export async function getServerSideProps(context) {
+  context.res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=1800, stale-while-revalidate=86400"
+  );
+  const allPostsData = getSortedPostsData();
+
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
@@ -21,9 +26,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm a Full Stack Engineer at Beamible. I love React and building cool UIs.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          I'm a Full Stack Engineer at Beamible. I love React and building cool
+          UIs.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -44,5 +52,5 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
